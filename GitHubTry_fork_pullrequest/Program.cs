676eb23a22a10334c47f8f865631f8a0ac443c76
Acer <Acer@DESKTOP-ABKHEEV>
2024-12-24 08:45:30 +0530

    Add project files.

---
+++ GitHubTry_fork_pullrequest/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace GitHubTry_fork_pullrequest
+{
+    class calculator
+    {
+        public void sum(int x, int y)
+        {
+            Console.WriteLine(x + y);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            calculator c1 = new calculator();
+            c1.sum(10, 20);
+        }
+    }
+}
