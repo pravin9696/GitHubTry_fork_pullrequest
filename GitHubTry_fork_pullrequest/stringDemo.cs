164b12d9f476cd78830a9755774f640855eef04a
Acer <Acer@DESKTOP-ABKHEEV>
2024-12-24 11:37:20 +0530

    new class stringdemo added

---
+++ GitHubTry_fork_pullrequest/stringDemo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubTry_fork_pullrequest
+{
+    internal class stringDemo
+    {
+        public void show()
+        {
+            Console.WriteLine("welcome to my project");
+        }
+    }
+}
